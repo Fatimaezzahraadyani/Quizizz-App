c11fc5de7a0485a5eb6957b5e2bdd96e9138659a
fatimaezzahraadyani <fatimaezzahraadyani@gmail.com>
2025-04-16 10:59:02 +0200

    carte pour entrer le nom de l'utilisateur/ Home component

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
   imports: [],
   templateUrl: './homepage.component.html',
-  styleUrl: './homepage.component.css'
+  styleUrl: './homepage.component.css',
+  encapsulation : ViewEncapsulation.None
 })
 export class HomepageComponent {
 
